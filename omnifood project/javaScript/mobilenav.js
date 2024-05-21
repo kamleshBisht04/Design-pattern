b7d6f1605f65391aa03680ebc436de73f2795def
Kamlesh Bisht <kamleshbisht04@gmail.com>
2024-05-21 09:12:16 +0530

    adding java script updating the copyright section the year upto date

---
+++ omnifood project/javaScript/mobilenav.js
@@ -0,0 +1,16 @@
+"using strict";
+
+
+///////////////////////////////////////////////////////////
+// Make updating the copyright year section up to date
+
+const yearEl = document.querySelector('.year');
+const currentYear = new Date().getFullYear();
+yearEl.textContent =currentYear;
+
+
+
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
+
